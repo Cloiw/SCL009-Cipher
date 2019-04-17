16bcb2a3c117a9b1f374a38361c7e14eeb6d43fa
Cloiw <sircli@hotmail.com>
2019-04-17 10:12:49 -0400

    Numeros

--- src/cipher.js
+++ src/cipher.js
@@ -52,10 +52,14 @@ result+=String.fromCharCode(cipMath);
 result += " ";
 
 }else if (number >= 97 && number <=122){//letras minusculas
-cipMath = (number-45-offset)%26+97;
+cipMath = (number-122-offset)%26+122;
 result+=String.fromCharCode(cipMath);
 
+}else if(number >=48 && number<=57){//numeros
+  cipMath = (number-57-offset)%10+57;
+  result+=String.fromCharCode(cipMath);
 }
+
 else if (number === 209){//ñ mayuscula
 result+='Ñ';
 }
